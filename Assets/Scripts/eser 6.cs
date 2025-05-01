0861c387af7befb6057d30de5341b76695538cb5
Andrea-rossi-15 <andrearossi.1507@gmail.com>
2025-05-01 14:43:26 +0200

    .

--- Assets/Scripts/eser 6.cs
+++ Assets/Scripts/eser 6.cs
@@ -13,43 +13,48 @@ public class eser6 : MonoBehaviour
     void Start()
     {
 
+        int[] arr = new int[20];
 
 
         {
+            for (int i = 0; i < arr.Length; i++)
+            {
 
 
-            int GeneraArray = Random.Range(1, 101);
+                int GeneraArray = Random.Range(1, 101);
+
+                arr[0] = GeneraArray;
+                arr[1] = GeneraArray;
+                arr[2] = GeneraArray;
+                arr[3] = GeneraArray;
+                arr[4] = GeneraArray;
+                arr[5] = GeneraArray;
+                arr[6] = GeneraArray;
+                arr[7] = GeneraArray;
+                arr[8] = GeneraArray;
+                arr[9] = GeneraArray;
+                arr[10] = GeneraArray;
+                arr[11] = GeneraArray;
+                arr[12] = GeneraArray;
+                arr[13] = GeneraArray;
+                arr[14] = GeneraArray;
+                arr[15] = GeneraArray;
+                arr[16] = GeneraArray;
+                arr[17] = GeneraArray;
+                arr[18] = GeneraArray;
+                arr[19] = GeneraArray;
 
 
-            int[] arr = new int[20];
-            arr[0] = GeneraArray;
-            arr[1] = GeneraArray;
-            arr[2] = GeneraArray;
-            arr[3] = GeneraArray;
-            arr[4] = GeneraArray;
-            arr[5] = GeneraArray;
-            arr[6] = GeneraArray;
-            arr[7] = GeneraArray;
-            arr[8] = GeneraArray;
-            arr[9] = GeneraArray;
-            arr[10] = GeneraArray;
-            arr[11] = GeneraArray;
-            arr[12] = GeneraArray;
-            arr[13] = GeneraArray;
-            arr[14] = GeneraArray;
-            arr[15] = GeneraArray;
-            arr[16] = GeneraArray;
-            arr[17] = GeneraArray;
-            arr[18] = GeneraArray;
-            arr[19] = GeneraArray;
+                
 
+                int SommaArray = 0;
+                SommaArray = SommaArray + i;
+                Debug.Log(SommaArray + arr[i]);
 
+                int StampaArray = Random.Range(arr[0], arr[19]);
+                Debug.Log(StampaArray);
+            }
 
-            int SommaArray = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8] + arr[9] + arr[10] + arr[11] + arr[12] + arr[13] + arr[14] + arr[15] + arr[16] + arr[17] + arr[18] + arr[19];
-            Debug.Log(SommaArray);
-
-            int StampaArray = Random.Range(arr[0], arr[19]);
-            Debug.Log(StampaArray);
 
 
         }
